0b3c92745137b20732c32ce7208b9c03b41fcd5c
EmersonWarwick <emersonwarwick.github@emersonwarwick.com>
2022-11-13 19:45:09 +0000

    Initial

---
+++ UWP/MicrophoneController/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.Devices;
+using Windows.Media.Capture;
+using System.Threading.Tasks;
+using Windows.UI;
+using System.Threading;
+using Windows.UI.Core;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MicrophoneController
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        AudioDeviceController adc;
+        SolidColorBrush red = new SolidColorBrush(Color.FromArgb(200, 245, 0, 0));
+        SolidColorBrush green = new SolidColorBrush(Color.FromArgb(200, 0, 205, 0));
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Task.Run(() => GetAudioDeviceController());
+        }
+
+        private void MicrophoneControl_Click(object sender, RoutedEventArgs e)
+        {
+            if (adc != null)
+            {
+                adc.Muted = !adc.Muted;
+                this.MicrophoneControl.Background = adc.Muted ? red : green;
+            }
+        }
+        private async void GetAudioDeviceController()
+        {
+            MediaCapture mediaCapture = new MediaCapture();
+            await mediaCapture.InitializeAsync(new MediaCaptureInitializationSettings
+            {
+                MediaCategory = MediaCategory.Speech,
+                StreamingCaptureMode = StreamingCaptureMode.Audio
+            });
+            adc = mediaCapture.AudioDeviceController;
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                this.MicrophoneControl.Background = adc.Muted ? red : green;
+            });
+        }
+    }
+}
---
+++ WPF/MicrophoneController2/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio.CoreAudioApi;
+
+namespace MicrophoneController2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool isMicMuted;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Set Mute to true at start
+            SetMic(true);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Set Mute opposite to current status
+            SetMic(!isMicMuted);
+        }
+
+
+        private void SetMic(bool targetMicMuteCondition)
+        {
+            isMicMuted = targetMicMuteCondition;
+
+            MMDeviceEnumerator MMDE = new MMDeviceEnumerator();
+            MMDeviceCollection devices = MMDE.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+            foreach (var device in devices)
+            {
+                device.AudioEndpointVolume.Mute = isMicMuted;
+            }
+
+            // Change button colour
+            MicButton.Background = isMicMuted ? Brushes.Red : Brushes.Green;
+        }
+    }
+}
